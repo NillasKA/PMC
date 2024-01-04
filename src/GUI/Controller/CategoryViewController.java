283966a6a7e738fc7a042c4bea5897f5e6786611
NillasKA <kramernicklas@gmail.com>
2024-01-04 12:03:56 +0100

    for catching up purposes

--- src/GUI/Controller/CategoryViewController.java
+++ src/GUI/Controller/CategoryViewController.java
@@ -1,4 +1,16 @@
 package GUI.Controller;
 
+import GUI.Model.CatModel;
+import GUI.Model.MovieModel;
+
 public class CategoryViewController {
+    MovieModel movieModel;
+
+    public CategoryViewController(){
+        try {
+            movieModel = MovieModel.getInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/GUI/Controller/MainViewController.java
+++ src/GUI/Controller/MainViewController.java
@@ -1,4 +1,25 @@
 package GUI.Controller;
 
+import GUI.Model.CatModel;
+import javafx.event.ActionEvent;
+
 public class MainViewController {
+    CatModel catModel;
+
+    public MainViewController(){
+        try {
+            catModel = CatModel.getInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clickBrowse(ActionEvent actionEvent) {
+    }
+
+    public void clickCreate(ActionEvent actionEvent) {
+    }
+
+    public void clickDelete(ActionEvent actionEvent) {
+    }
 }
