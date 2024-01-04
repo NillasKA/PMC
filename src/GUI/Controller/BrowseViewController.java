61251cf2e0cc1b35bc350898fa7b2916a28eca57
NillasKA <kramernicklas@gmail.com>
2024-01-04 11:43:56 +0100

    Catchup

--- src/GUI/Controller/BrowseViewController.java
+++ src/GUI/Controller/BrowseViewController.java
@@ -1,15 +1,4 @@
 package GUI.Controller;
 
-import GUI.Model.MovieModel;
-
 public class BrowseViewController {
-    MovieModel movieModel;
-
-    public BrowseViewController(){
-        try {
-            movieModel = MovieModel.getInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
--- src/GUI/Controller/CategoryViewController.java
+++ src/GUI/Controller/CategoryViewController.java
@@ -1,16 +1,4 @@
 package GUI.Controller;
 
-import GUI.Model.CatModel;
-import GUI.Model.MovieModel;
-
 public class CategoryViewController {
-    MovieModel movieModel;
-
-    public CategoryViewController(){
-        try {
-            movieModel = MovieModel.getInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
