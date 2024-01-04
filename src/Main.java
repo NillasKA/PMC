cf915b2e3ec555deb682c634686ad45c4339af39
Gergö Kadar <kadargergo20@gmail.com>
2024-01-04 11:36:27 +0100

    Startup kode

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,29 @@ import BE.Movie;
 import GUI.Model.CatModel;
 import GUI.Model.CatMovieModel;
 import GUI.Model.MovieModel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
-public class Main {
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource("view/mainView.fxml"));
+
+        primaryStage.setTitle("Private Movie Collection");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) throws Exception {
-        //TEST ITEMS TO EASILY TEST IT ALL.
+        launch(args);
         MovieModel movieModel = MovieModel.getInstance();
         CatModel catModel = CatModel.getInstance();
         CatMovieModel catMovieModel = CatMovieModel.getInstance();
