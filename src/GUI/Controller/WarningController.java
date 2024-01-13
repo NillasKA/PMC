12c1a6cd30b6dde9d61be18605199b5411d545c2
VictorBjerrumvb <144001219+VictorBjerrumvb@users.noreply.github.com>
2024-01-13 18:48:08 +0100

    warning window fixed
    asd

--- src/GUI/Controller/WarningController.java
+++ src/GUI/Controller/WarningController.java
@@ -4,7 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import utility.PMCException;
 
 import java.io.IOException;
@@ -12,27 +14,34 @@ import java.io.IOException;
 public class WarningController {
     public void understoodBtn(ActionEvent actionEvent) throws PMCException {
         try {
+            // Load the MainView.fxml
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainView.fxml"));
-            Parent newWindow = loader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Warning");
-            stage.setScene(new Scene(newWindow));
+            Parent root = loader.load();
 
-            MainViewController controller = loader.getController();
-            controller.setParentController(this);
-            controller.setStage(stage);
-            
-            stage.showAndWait();
+            // Get the reference to the button's window
+            Window currentWindow = ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
 
-            controller.closeStage();
+            // Cast the window to Stage
+            Stage currentStage = (Stage) currentWindow;
+
+            // Close the current stage (warning window)
+            currentStage.close();
+
+            // Create a new instance of BorderPane for the MainView scene
+            BorderPane mainRoot = new BorderPane();
+            mainRoot.setCenter(root);
+
+            // Set the new scene for the current stage
+            currentStage.setScene(new Scene(mainRoot));
+
+            // Show the current stage (MainView)
+            currentStage.show();
         } catch (IOException e) {
-            throw new PMCException("Could not load new Stage", e);
+            throw new PMCException("Could not load MainView", e);
         }
     }
-
-    private void setStage(Stage stage) {
-    }
-
-    private void setParentController(WarningController warningController) {
-    }
 }
+
+
+
+
